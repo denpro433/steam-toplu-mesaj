6df4f410d2f1558aecf62ca9e1050785de2c37ae
denpro433 <68608964+denpro433@users.noreply.github.com>
2020-12-09 19:13:08 +0300

    Add files via upload

---
+++ Setup.bat
@@ -0,0 +1,31 @@
+@echo off
+Color a
+echo "Modller ykleniyor..;"
+call npm install
+Color a
+echo " Ykleme tamamlandi steam user ykleniyor."
+Color a
+call npm install steam-user
+
+echo "ykleme tamamland steamcommunity ykleniyor."
+Color a
+call npm install steamcommunity
+
+echo "ykleme tamamland colors ykleniyor."
+Color a
+call npm install colors
+echo "ykleme tamamland readline ykleniyor."
+Color a
+call npm install readline
+echo "ykleme tamamland fs ykleniyor."
+Color a
+call npm install fs
+echo"ykleme tamamland steam-userinfo ykleniyor."
+Color a
+call npm install steam-userinfo
+echo"ykleme tamamland steam-totp ykleniyor."
+Color a
+call npm install steam-totp
+echo "ykleme tamamlandi."
+Color a
+pause
---
+++ toplumesaj.js
@@ -0,0 +1,127 @@
+const SteamUser = require('steam-user');
+var SteamCommunity = require('steamcommunity');
+const community = new SteamCommunity();
+var client = new SteamUser();
+var colors = require('colors');
+const readline = require('readline');
+const SteamTotp = require('steam-totp');
+const fs = require('fs');
+var steamuserinfo = require('steam-userinfo');
+const config = require('./config.json');
+ var isim;
+ var inlist=false;
+steamuserinfo.setup(config.api);
+client.logOn({
+    "accountName": config.account.username,
+    "password": config.account.password,
+	"twoFactorCode": SteamTotp.generateAuthCode(config.account.twofactorcode)
+	
+});
+client.on('loggedOn', async function(details) {
+	
+	console.log("giriş başarili".green);
+		  	
+	
+});
+var blacklist=[];
+var mesaj=[];
+
+let rl = readline.createInterface({
+    input: fs.createReadStream('blacklist.txt')
+});
+
+rl.on('line', function(line) {
+	
+	blacklist.push(line);
+})
+
+
+var mesajc;
+fs.readFile('mesaj.txt', 'utf8', function(err, data) {
+  if (err) throw err;
+owo=data.split(']');
+  mesaj.push(owo[0]);
+  //console.log(data)
+  //mesajc=data;
+});
+
+client.on("webSession", async function(sessionID, cookies,err)  {   
+
+if(err)
+{
+	throw err;
+}
+
+var msg;
+
+
+
+
+
+var date = new Date();
+
+  // TODO: Log the answer in a database
+  console.log(`Tüm kullanıcılara gönderilecek mesaj ${mesaj[0]}`.blue);
+
+
+
+			
+				console.log("toplam listeniz "+ Object.keys(client.myFriends).length);
+				
+		   for (let i = 0; i < Object.keys(client.myFriends).length; ) {
+		
+
+
+        	var idlen=Object.keys(client.myFriends)[i];
+			
+			var steam64id=idlen;
+				steamuserinfo.getUserInfo(steam64id,async function(error, data){
+			if (err) throw err;
+			 isim=data.response.players[0].personaname;
+			 var id=data.response.players[0].steamid;
+			 
+			 var ülkesi=data.response.players[0].loccountrycode;
+			 inlist=false;
+			 for(var a=0;a<=blacklist.length;a++)
+			 {
+			if(blacklist[a].includes(isim))
+			{
+				inlist=true;
+			// console.log(isim+' '+ülkesi);
+			
+			}
+			 }
+			 if(inlist==false)
+			 {
+			 
+			  if(ülkesi=='TR'||ülkesi==undefined)
+			 {
+		client.chatMessage(id,mesaj[0]);
+			var current_hour = date.getHours()+":"+date.getMinutes();
+			 console.log(`Mesaj gönderilen kisi: ${isim} Mesaj: ${mesaj[0]} ${[current_hour]}`.green);
+			 console.log("5 saniye sonra yeni mesaj atılacaktır".red)
+		
+		
+			 }
+			 else{
+				
+			 }
+			 }
+			 
+
+		
+
+		 });
+		  await new Promise(r => setTimeout(r, 5000));
+		  i++;
+  
+    }
+	console.log("Tüm Mesaj gönderimleri tamamlandı 10 saniye sonra çıkış yapılacaktir".green);
+	setTimeout(process.exit, 10000, 1).unref();
+	
+	
+	
+			
+			
+			
+})
